e5138327f4d5c13c411280a697f47756fee28792
samskinnerphd <samskinnerphd@gmail.com>
2017-03-13 12:56:46 -0400

    clean up routes and controllers

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -4,49 +4,4 @@ class LettersController < ApplicationController
 
   def bitovi
   end
-
-  def kaleo
-  end
-
-  def olark
-  end
-
-  def tax_jar
-  end
-
-  def deal_dash
-  end
-
-  def white_spectre
-  end
-
-  def rea
-  end
-
-  def catapult
-  end
-
-  def general
-  end
-
-  def invision
-  end
-
-  def stembolt
-  end
-
-  def samasource
-  end
-
-  def optimizely
-  end
-
-  def copart
-  end
-
-  def jaguar_design
-  end
-
-  def flexport
-  end
 end
