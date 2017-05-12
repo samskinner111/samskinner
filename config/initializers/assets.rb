777927070e3fc389652c5e8081a1da32523182af
samskinnerphd <samskinnerphd@gmail.com>
2017-05-12 14:21:38 -0400

    add resume & academicons to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 Rails.application.config.assets.version = '1.0'
 
 # Stylesheets
-Rails.application.config.assets.precompile += ['navbar.scss', 'home.scss', 'letters.scss']
+Rails.application.config.assets.precompile += ['navbar.scss', 'home.scss', 'letters.scss', 'resume.scss', 'academicons.css']
 
 Rails.application.config.assets.precompile += %w[about-header.jpg base-jumping.png favicon.ico
   my-girls.png nasonia.jpg personal-picture.png polychaete.jpg resume-header.jpg SS-logo.png
